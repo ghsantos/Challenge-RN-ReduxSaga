33cf315a09928d663a2911e0b1a0b3fd6c7d2813
ghsantos <ghsantos933@gmail.com>
2018-12-01 19:36:09 -0200

    redux setup

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,14 +2,14 @@ import { put, takeLatest, all } from 'redux-saga/effects';
 
 import { getBooks } from '../api';
 
-function* fetchBookes() {
+function* fetchBooks() {
   const books = yield getBooks();
 
   yield put({ type: 'BOOKS_RECEIVED', books });
 }
 
 function* actionWatcher() {
-  yield takeLatest('GET_BOOKS', fetchBookes);
+  yield takeLatest('GET_BOOKS', fetchBooks);
 }
 
 export default function* rootSaga() {
