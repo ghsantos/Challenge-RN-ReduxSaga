bffca8c994c9585a928f89e84e3350f3a0d93d02
ghsantos <ghsantos933@gmail.com>
2018-12-02 14:38:27 -0200

    update Like button

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,27 +1,44 @@
 /* @flow */
 
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default class LikeButton extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
+const Touchable =
+  Platform.Version >= 21 ? TouchableNativeFeedback : TouchableOpacity;
+
+const LikeButton = ({ onPress }) => (
+  <View style={styles.container}>
+    <Touchable
+      onPress={() => setTimeout(() => onPress(), 0)}
+      background={TouchableNativeFeedback.Ripple('', true)}
+    >
+      <View style={styles.content}>
         <Icon name="ios-heart-empty" size={25} color="#fff" />
       </View>
-    );
-  }
-}
+    </Touchable>
+  </View>
+);
+
+export default LikeButton;
 
 const styles = StyleSheet.create({
   container: {
     height: 42,
     width: 42,
-    justifyContent: 'center',
-    alignItems: 'center',
     elevation: 4,
     borderRadius: 21,
     backgroundColor: '#DC4B5D',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- src/scenes/Detail.js
+++ src/scenes/Detail.js
@@ -86,7 +86,7 @@ class Detail extends Component {
                   <Button onPress={() => {}} text="BUY" />
                 </View>
 
-                <LikeButton />
+                <LikeButton onPress={() => {}} />
               </View>
             </View>
           </View>
