4e70d6f7aea98f76035bac7584b635d18849e717
ghsantos <ghsantos933@gmail.com>
2018-12-02 16:00:49 -0200

    update Home screen

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 import Button from '../components/Button';
 import Header from '../components/Header';
@@ -14,10 +15,16 @@ export default class Home extends Component {
       <View style={styles.container}>
         <Header center={<Text style={styles.headerTitle}>Books</Text>} />
 
-        <Button
-          onPress={() => this.props.navigation.navigate('List')}
-          text="Go to List"
-        />
+        <View style={styles.iconContainer}>
+          <Icon name="book" size={180} color="#ddd" />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={() => this.props.navigation.navigate('List')}
+            text="List Books"
+          />
+        </View>
       </View>
     );
   }
@@ -29,5 +36,15 @@ const styles = StyleSheet.create({
   },
   headerTitle: {
     fontSize: 20,
+  },
+  iconContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
   },
 });
